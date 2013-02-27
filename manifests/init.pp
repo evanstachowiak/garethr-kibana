88c85c1e34b24b0456b655a03bbb701b180c5a03
Evan Stachowiak <evan.stachowiak@chloeandisabel.com>
2013-02-27 11:36:23 -0500

    update anchor pattern for params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class kibana (
   $ensure            = 'present',
   $git_hash          = $kibana::params::git_hash,
+  $git_source        = $kibana::params::git_source,
   $install_dir       = $kibana::params::install_dir,
   $install_dir_mode  = $kibana::params::install_dir_mode,
   $install_dir_owner = $kibana::params::install_dir_owner,
@@ -11,12 +12,12 @@ class kibana (
   vcsrepo { '/opt/kibana':
     ensure   => $ensure,
     provider => 'git',
-    source   => 'git://github.com/rashidkpc/Kibana.git',
+    source   => $git_source,
     revision => $git_hash,
   }
 
+  Class['kibana'] ->
   class { 'kibana::config': } ~>
   class { 'kibana::service': }
-  anchor { 'kibana::begin': } -> Class ['kibana::params']
-  Class['kibana::service'] -> anchor { 'kibana::end': }
+  anchor { 'kibana::begin': } -> Class['kibana::params'] -> anchor { 'kibana::end': }
 }

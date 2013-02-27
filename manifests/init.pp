0cb02157a2ea7416549b3d1f82c3e3e2a37f8e54
Evan Stachowiak <evan.stachowiak@chloeandisabel.com>
2013-02-26 19:42:09 -0500

    add anchor pattern to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,8 @@ class kibana(
 ){
   class { 'kibana::install': } ->
   class { 'kibana::config': } ~>
-  class { 'kibana::service': } ->
-  Class['kibana']
+  class { 'kibana::service': }
+
+  anchor { 'kibana::begin': } -> Class ['kibana::install']
+  Class['kibana::service'] -> anchor { 'kibana::end': }
 }

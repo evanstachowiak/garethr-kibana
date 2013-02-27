35fec1dc0b5463a4cc4a8bae61013550bf855ab0
Evan Stachowiak <evan.stachowiak@chloeandisabel.com>
2013-02-27 10:50:52 -0500

    refactor init/install and add params class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,31 @@
-class kibana(
-  $git_hash = 'edb4553',
-){
-  class { 'kibana::install': } ->
+class kibana (
+  $ensure            = 'present',
+  $git_hash          = $kibana::params::git_hash,
+  $install_dir       = $kibana::params::install_dir,
+  $install_dir_mode  = $kibana::params::install_dir_mode,
+  $install_dir_owner = $kibana::params::install_dir_owner,
+  $install_dir_group = $kibana::params::install_dir_group
+) inherits kibana::params {
+  $ensure_dir = $ensure ? { 'present' => 'directory', default => $ensure }
+
+  File['kibana_install_dir'] ->
+  Vcsrepo['/opt/kibana']
+  file { 'kibana_install_dir':
+    ensure => $ensure_dir,
+    path   => $install_dir,
+    mode   => $install_dir_mode,
+    owner  => $install_owner,
+    group  => $install_group,
+  }
+  vcsrepo { '/opt/kibana':
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'git://github.com/rashidkpc/Kibana.git',
+    revision => $git_hash,
+  }
+
   class { 'kibana::config': } ~>
   class { 'kibana::service': }
-
-  anchor { 'kibana::begin': } -> Class ['kibana::install']
+  anchor { 'kibana::begin': } -> Class ['kibana::params']
   Class['kibana::service'] -> anchor { 'kibana::end': }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+class kibana::params {
+
+  case $::operatingsystem {
+    /(?i-mx:debian|ubuntu)/: {
+      $git_hash = '09d04ab79032b427cf6e5f771871ffc6a85c3a00'
+      $install_dir       = '/opt/kibana'
+      $install_dir_mode  = '0775'
+      $install_dir_owner = 'root'
+      $install_dir_group = 'root'
+  }
+    default: {
+      fail("Unsupported platform: ${::operatingsystem}")
+    }
+  }
+  
+}
